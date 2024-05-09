db816c2a9698fc623a1eca37443fc309c84ef5ee
Vladimir Mitrovic <vladimir.mitrovic@gmail.com>
2024-05-09 17:25:38 +0200

    Fix multiprocessing example.

--- producer/src/main_multiprocessing.py
+++ producer/src/main_multiprocessing.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import time
 from faker import Faker
 from typer import Typer, prompt, echo
@@ -6,76 +8,106 @@ from confluent_kafka import Producer
 import orjson
 
 app = Typer()
-fake = Faker()
+faker = Faker()
 
-producer = Producer({'bootstrap.servers': 'kafka:29092'})
+producer = Producer({"bootstrap.servers": "kafka:29092"})
+num_cores_to_use = max(os.cpu_count() - 1, 1)
 
 total_messages_failed = 0
 total_messages_sent = 0
 total_time_taken = 0
+chunk = 1
+
 
 def generate_message(_):
     message = {
-        'name': fake.name(),
-        'email': fake.email(),
-        'address': fake.address(),
-        'timestamp': fake.date_time().isoformat(),
-        'date': fake.date(),
-        'time': fake.time(),
-        'age': fake.random_int(min=18, max=65),
-        'price': fake.random_element(elements=(100, 99.99, '49.99')),
-        'description': fake.text(),
-        'title': fake.catch_phrase(),
-        'category': fake.random_element(elements=('Books', 'Electronics', 'Clothing')),
-        'latitude': str(fake.latitude()),
-        'longitude': str(fake.longitude()),
-        'is_active': fake.random_element(elements=(True, False)),
-        'is_completed': fake.random_element(elements=(1, 0)),
-        'status': fake.random_element(elements=('pending', 'in progress', 'completed'))
+        "name": faker.name(),
+        "email": faker.email(),
+        "address": faker.address(),
+        "timestamp": faker.date_time().isoformat(),
+        "date": faker.date(),
+        "time": faker.time(),
+        "age": faker.random_int(min=18, max=65),
+        "price": faker.random_element(elements=(100, 99.99, "49.99")),
+        "description": faker.text(),
+        "title": faker.catch_phrase(),
+        "category": faker.random_element(elements=("Books", "Electronics", "Clothing")),
+        "latitude": str(faker.latitude()),
+        "longitude": str(faker.longitude()),
+        "is_active": faker.random_element(elements=(True, False)),
+        "is_completed": faker.random_element(elements=(1, 0)),
+        "status": faker.random_element(
+            elements=("pending", "in progress", "completed")
+        ),
     }
     return orjson.dumps(message)
 
+
 def send_messages(messages):
-    global producer
+    global chunk, producer, total_messages_failed, total_messages_sent
     try:
         for message in messages:
-            producer.produce('my-topic', message)
+            producer.produce("my-topic", message)
         producer.flush()
+        echo(f"Message sent successfully | Chunk: {chunk})")
+        chunk += 1
+        total_messages_sent += len(messages)
         return True
     except Exception as e:
-        echo(f"Error sending messages: {e}")
+        echo(f"Error sending message: {e}")
+        total_messages_failed += len(messages)
         return False
 
+
 def produce_messages(num_messages: int, batch_size: int):
-    global total_messages_failed, total_messages_sent, total_time_taken
+    global total_time_taken
     start_time = time.time()
-    with Pool(processes=4) as pool:
+    with Pool(processes=num_cores_to_use) as pool:
         for _ in range(0, num_messages, batch_size):
             messages = pool.map(generate_message, range(batch_size))
             send_messages(messages)
-            total_messages_sent += batch_size
     end_time = time.time()
     total_time_taken = end_time - start_time
 
+
 @app.command()
 def run():
-    start_producing = prompt("Would you like to start producing messages? (Yes/No): ", default="Yes")
+    start_producing = prompt(
+        "Would you like to start producing messages? (Yes/No): ", default="Yes"
+    )
     if start_producing.lower() == "yes":
-        num_messages = prompt("Enter the number of messages to produce: ", type=int, default=100)
+        num_messages = prompt(
+            "Enter the number of messages to produce: ", type=int, default=100
+        )
         batch_size = prompt("Enter the batch size: ", type=int, default=10)
         try:
             produce_messages(num_messages, batch_size)
         except KeyboardInterrupt:
             pass
+        finally:
+            report()
+    else:
+        echo("Exiting...")
+        sys.exit()
 
-    echo("Exiting...")
-    shutdown()
+    echo("\n\nRe-running...")
+    run()
 
-def shutdown():
-    global total_messages_failed, total_messages_sent, total_time_taken
+
+def report():
     echo(f"\nTotal Messages Failed: {total_messages_failed}")
     echo(f"Total Messages Sent: {total_messages_sent}")
     echo(f"Total Time Taken: {total_time_taken:.4f} seconds")
+    reset_counters()
+
+
+def reset_counters():
+    global chunk, total_messages_failed, total_messages_sent, total_time_taken
+    total_messages_failed = 0
+    total_messages_sent = 0
+    total_time_taken = 0
+    chunk = 1
+
 
 if __name__ == "__main__":
     app()
