9c0f8545663eb68c2c60bbb1103f9e4a06b16e67
Vladimir Mitrovic <vladimir.mitrovic@gmail.com>
2024-05-11 01:53:53 +0200

    Add Message model.

--- consumer/src/model.py
+++ consumer/src/model.py
@@ -1,37 +1,14 @@
-from datetime import datetime
+import uuid
 from sqlalchemy.orm import registry
 from sqlmodel import SQLModel, Field
+from sqlalchemy.dialects.postgresql import JSONB
 
 
 class BaseModel(SQLModel, registry=registry()):
     pass
 
 
-# @TODO Check if onupdate=datetime.utcnow is needed for updated_at field.
-# I'm not sure what default_factory does.
-class TimeStampMixin(SQLModel):
-    created_at: datetime | None = Field(default=datetime.utcnow(), nullable=False)
-    updated_at: datetime | None = Field(default_factory=datetime.utcnow, nullable=False)
-
-
-# class TimestampModel(SQLModel):
-#    created_at: datetime = Field(
-#        default_factory=datetime.utcnow,
-#        nullable=False,
-#        sa_column_kwargs={
-#            "server_default": text("current_timestamp(0)")
-#        }
-#    )
-
-#    updated_at: datetime = Field(
-#        default_factory=datetime.utcnow,
-#        nullable=False,
-#        sa_column_kwargs={
-#            "server_default": text("current_timestamp(0)"),
-#            "onupdate": text("current_timestamp(0)")
-#        }
-#    )
-
-
-# Load models:
-# from src.auth.models import *  # noqa
+class Message(BaseModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    key: str | None = Field(nullable=True)
+    value: dict | None = Field(nullable=True, sa_type=JSONB)
