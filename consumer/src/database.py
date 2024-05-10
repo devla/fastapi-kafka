4bf45ca96306dab3c4b5a715cfc4aade80039bdc
Vladimir Mitrovic <vladimir.mitrovic@gmail.com>
2024-05-11 01:53:06 +0200

    Update database connection logic.

--- consumer/src/database.py
+++ consumer/src/database.py
@@ -12,19 +12,31 @@ async_engine = create_async_engine(
     settings.DATABASE_URL,
     echo=settings.DB_ECHO,
     future=settings.DB_FUTURE,
+    pool_size=max(5, settings.DB_POOL_SIZE),
 )
+
+SessionLocal = sessionmaker(
+    bind=async_engine,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+
+# Dependency to get a database session per request
+async def get_async_session():
+    async with SessionLocal() as session:
+        try:
+            yield session
+        except Exception as e:
+            await session.rollback()
+            raise e
+        finally:
+            await session.close()
 
 
 # Define an async function to create tables
 async def create_tables():
     async with async_engine.begin() as conn:
         await conn.run_sync(BaseModel.metadata.create_all)
-
-
-# Dependency to get a database session
-async def get_async_session() -> AsyncSession:
-    async_session = sessionmaker(
-        bind=async_engine, class_=AsyncSession, expire_on_commit=False
-    )
-    async with async_session() as session:
-        yield session
