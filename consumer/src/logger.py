c63ca6bdb1a597dfeaed4bfc5510325f8acbeb41
Vladimir Mitrovic <vladimir.mitrovic@gmail.com>
2024-05-12 19:06:18 +0200

    Add logger.

---
+++ consumer/src/logger.py
@@ -0,0 +1,8 @@
+import os
+import logging
+
+
+logging.basicConfig(
+    level=getattr(logging, os.getenv("LOGLEVEL", "").upper(), "INFO"),
+    format="[%(asctime)s] %(levelname)s:%(name)s:%(message)s",
+)
